39ec8e2c7c8c3f47cee25c9d8dc728aa37749156
Tian <Tian@Tian.com>
2022-10-08 21:39:01 +0800

    添加redis环境的配置

---
+++ src/main/java/com/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis配置类，可写可不写，主要是修改spring默认提供的序列化器，不修改的话在自己连上cli客户端查看的keys会有乱码，修改后用cli查看就不会有乱码，
+ * 无论是用这个序列化器还是用spring默认的序列话器都不影响java中使用redis。
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
